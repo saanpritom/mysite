3f1f0ee13180b2080d9e333614dfb75654e96f1e
saanpritom <pritom.saan@gmail.com>
2018-03-13 14:25:15 +0600

    All templates, views and base.html

--- blog/urls.py
+++ blog/urls.py
@@ -10,4 +10,8 @@ urlpatterns = [
     url(r'^post/(?P<pk>\d+)/edit$', views.UpdatePostView.as_view(), name='post_edit'),
     url(r'^post/(?P<pk>\d+)/delete$', views.DeletePostView.as_view(), name='post_delete'),
     url(r'^draft$', views.DraftListView.as_view(), name='post_draft'),
+    url(r'^post/(?P<pk>\d+)/publish$', views.post_publish, name='post_publish'),
+    url(r'^post/(?P<pk>\d+)/comment$', views.add_comment_to_post, name='add_comment_to_post'),
+    url(r'^comment/(?P<pk>\d+)/approve$', views.comment_approve, name='comment_approve'),
+    url(r'^comment/(?P<pk>\d+)/remove$', views.comment_delete, name='comment_delete'),
 ]
